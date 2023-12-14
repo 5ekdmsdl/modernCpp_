21bfdf4a4f2850af17055bca11efa43c4f724444
daeun <5ekdmsdldl@gmail.com>
2023-12-14 17:21:56 +0900

    부분 특수화 template partial specialization

--- DAY4/5_부분특수화0.cpp
+++ DAY4/5_부분특수화0.cpp
@@ -11,8 +11,23 @@ int p = 0;
 template<typename T> void foo(T a)
 {
 	// 아래 코드에서 * 의 의미를 생각해 보세요
-	Test::data  * p; 
-	Test::DWORD * p; 
+	// "클래스이름::이름" 에서 이름은
+	// "값" 일수도, "타입" 일수도 있지
+	Test::data  * p; // data 값 -> * : 곱하기
+	Test::DWORD * p; // DWORD 타입, *:포인터, p:포인터 변수	
+
+	// 아래 코드를 생각해보세요.
+	// template인 경우 -> "dependent name"
+	// => template 인자 T에 의존해서 꺼내는 이름
+	// => "T::이름" 에서 컴파일러 무조건 "값"으로 해석
+	T::data* p1;	// error. p1 없다.
+	T::DWORD* p2;	// error. p2 없다.
+					// 근데 이 코드의 의도는 포인터 p2의 선언
+
+	// 해결책
+	// dependent name이 타입으로 사용되게 하려면
+	// 컴파일러에게 알려줘야..
+	typename T::DWORD* p3;	// ok
 
 	
 }
--- DAY4/5_부분특수화1.cpp
+++ DAY4/5_부분특수화1.cpp
@@ -1,12 +1,33 @@
 // 3_부분특수화1 - 156 p
 #include <iostream>
 
+// 기본 틀 만들고, 
+// 상속 받아서 특수화 마다 세부 구현 반복 X
+class Base {};
 
-template<typename T> class Stack
+// primary template
+template<typename T> class Stack : Base
 {
 public:
 	void push(T a) { std::cout << "T" << std::endl; }
 };
+
+// partial specialization (부분 특수화)
+template<typename T> class Stack<T*> 
+{
+public:
+	void push(T a) { std::cout << "T*" << std::endl; }
+};
+
+// specialization (특수화)
+template<> class Stack<char*>
+{
+public:
+	void push(char* a) { std::cout << "char*" << std::endl; }
+};
+
+// 실제로 찍어낸 객체에 따라서만 template 사용됨
+// 특수화로 인한 오버헤드 X
 
 int main()
 {
@@ -14,3 +35,6 @@ int main()
 	Stack<int*>  s2; s2.push(0);
 	Stack<char*> s3; s3.push(0);
 }
+
+
+
