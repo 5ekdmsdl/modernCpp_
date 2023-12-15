ae1a0da7aba2faee052d40fe913d44a2a81bc28a
daeun <5ekdmsdldl@gmail.com>
2023-12-15 14:37:52 +0900

    utilize variadic arguments tech

--- DAY5/6_가변인자템플릿활용1.cpp
+++ DAY5/6_가변인자템플릿활용1.cpp
@@ -1,13 +1,53 @@
 #include <iostream>
 
+// 타입 : 선언문에서 "이름"을 제거하면 타입만 남게 됩니다.
+// int n    => 이름은 n, 타입 int
+// int x[3] => 이름은 x, 타입 int[3]
+
+
+// f 타입 : int (int, double)
 int f(int a, double b) { return 0; }
 
-template<typename T> void foo(T& a) 
-{
-	typename result<T>::type n;  
+// 함수 타입에서 "반환타입" 을 얻을수 있는 traits 만들기
 
-	cout << typeid(n).name() << endl; 
+// primary template
+template<typename T> struct result
+{
+	using type = T;
+};
+
+/*
+// 핵심 : int (int, double) 형태의 타입에서 반환타입(int) 을 얻을수 있도록
+//       부분 특수화 버전을 만들어야 합니다. 이부분이 핵심!!
+// primary template 의 template 인자는 한개 지만
+// 아래처럼 부분 특수화 할때는 template 인자의 갯수가 달라도 됩니다.
+
+template<typename R, typename A1, typename A2> struct result< R(A1, A2) >
+{
+	using type = R;
+};
+*/
+// 위처럼 하면 인자가 2개인 함수만 가능합니다.
+// 아래 처럼 하면 인자의 갯수에 제한이 없습니다.
+template<typename R, typename ... Types> struct result< R(Types...) >
+{
+	using type = R;
+};
+
+
+
+template<typename T> void foo(T& a)
+{
+	// T : int (int, double)
+	typename result<T>::type n;  // int
+
+	std::cout << typeid(n).name() << std::endl;
 }
+
+
+
+
+
 
 int main()
 {
--- DAY5/6_가변인자템플릿활용2.cpp
+++ DAY5/6_가변인자템플릿활용2.cpp
@@ -3,12 +3,65 @@
 int f(int, double, char) { return 0; }
 
 
+// 이번에는 반환타입이 아닌 인자의 타입을 구해 봅시다.
+// primary template
+template<int N, typename T>
+struct argument
+{
+	using type = T;
+};
+
+/*
+template<int N, typename R, typename ... Types>
+struct argument<N, R(Types...)>
+{
+	// ? 를 표기할수 있을까요 ?
+	// N 을 알수 없다... 잘못된 부분 특수화
+	using type = ?;
+};
+*/
+
+/*
+// N == 0
+template<typename R, typename ... Types>
+struct argument<0, R(Types...)>
+{
+	// R(Types...) 의 모양에서 0번째 인자의 타입을 구할수 없습니다.
+	// 잘못된 부분 특수화
+	using type = ? ;
+};
+*/
+
+// N == 0
+template<typename R, typename A, typename ... Types>
+struct argument<0, R(A, Types...)>
+{
+	using type = A;
+};
+
+// N != 0
+template<int N, typename R, typename A, typename ... Types>
+struct argument<N, R(A, Types...)>
+{
+	// int(int , double, char)의 1번째 인자 타입 (double)은
+	// int(    , double, char)의 0번째 인자 타입과 동일
+	// 
+	// => R(A, Types...)> 의  N 번째 인자의 타입은 
+	// => R(   Types...)> 의  N-1 번째 인자의 타입과 같습니다.
+	//
+	using type = argument<N-1, R(Types...)>::type;
+
+};
+
+
 
 template<typename T> void foo(T& t)
 {
-	typename argument<1, T>::type n;
+	// T : int (int, double, char)
+	//typename argument<0, T>::type n;	// int
+	typename argument<2, T>::type n;	// char
 
-	std::cout << typeid(n).name() << std::endl; 
+	std::cout << typeid(n).name() << std::endl;
 }
 
 int main()
